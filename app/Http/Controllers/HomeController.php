f16692d5c56e3e9162a11a95e376c3c1c5781f3a
m7mdbahaa <m7md.bahaa1@gmail.com>
2019-03-03 12:23:27 +0200

    merge feature/about branch

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EducationHistory;
+use App\Models\WorkExperiences;
 use Illuminate\Http\Request;
 use App\Models\Menus;
+use App\Models\Paragraphs;
+
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('home.index');
+        $paragraphs = Paragraphs::getParagraphs();
+        $educations = EducationHistory::all();
+        $experiences = WorkExperiences::orderBy('start_date')->get();
+
+        return view('home.index', compact('paragraphs','educations','experiences'));
 
     }
 }
